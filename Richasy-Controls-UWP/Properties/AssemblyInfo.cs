c72e6eda0ed049eaaf82a0e7f0502b275f1bfe9f
安然 张 <zar234@qq.com>
2020-06-28 12:10:32 +0800

    修改打包描述

--- Richasy-Controls-UWP/Properties/AssemblyInfo.cs
+++ Richasy-Controls-UWP/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Richasy-Controls-UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Richasy自用控件集，多是对原有控件的包装")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Richasy")]
 [assembly: AssemblyProduct("Richasy-Controls-UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
